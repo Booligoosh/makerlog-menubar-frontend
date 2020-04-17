1c706f598d41667c6ddeafd38373653d67f1f3cf
Ethan <me@ethan.link>
2020-04-17 15:03:20 +1000

    Hotfix to work with new netlify.app domains

--- auth-process-code.js
+++ auth-process-code.js
@@ -1,7 +1,7 @@
 var code = window.location.href.split('?code=')[1];
 console.log(code);
 
-fetch('https://makerlog-menubar-cloud-functions.netlify.com/.netlify/functions/exchangeMakerlogCode', {
+fetch('https://makerlog-menubar-cloud-functions.netlify.app/.netlify/functions/exchangeMakerlogCode', {
 	method: 'POST',
 	body: JSON.stringify({code: code})
 }).then(r => r.json()).then(r => {
